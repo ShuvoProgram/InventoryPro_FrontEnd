b543b2cc5bbe32577fcb5cbeb2209666153d782c
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-26 01:32:02 +0600

    Add or update ProductCreateUpdate component

---
+++ src/components/Product/ProductCreateUpdate.jsx
@@ -0,0 +1,184 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  CreateProductRequest,
+  FillProductFormRequest,
+  ProductBrandDropDownRequest,
+  ProductCategoryDropDownRequest,
+} from "../../APIRequest/ProductApiRequest";
+import { ErrorToast, IsEmpty } from "../../helper/FormHelper";
+import { OnChangeProductInput } from "../../redux/slice/productSlice";
+import store from "../../redux/store/store";
+
+const ProductCreateUpdate = () => {
+  let navigate = useNavigate();
+  let [ObjectID, SetObjectID] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      await ProductBrandDropDownRequest();
+      await ProductCategoryDropDownRequest();
+    })();
+
+    let params = new URLSearchParams(window.location.search);
+    let id = params.get("id");
+    if (id !== null) {
+      SetObjectID(id);
+      (async () => {
+        await FillProductFormRequest(id);
+      })();
+    }
+  }, []);
+
+  let FormValue = useSelector((state) => state.product.FormValue);
+  let BrandDropDown = useSelector((state) => state.product.BrandDropDown);
+  let CategoryDropDown = useSelector((state) => state.product.CategoryDropDown);
+
+  const SaveChange = async () => {
+    if (IsEmpty(FormValue.Name)) {
+      ErrorToast("Product Name Required !");
+    } else if (IsEmpty(FormValue.BrandID)) {
+      ErrorToast("Product Brand Required !");
+    } else if (IsEmpty(FormValue.CategoryID)) {
+      ErrorToast("Product Category Required !");
+    } else if (IsEmpty(FormValue.Unit)) {
+      ErrorToast("Product Unit Required !");
+    } else {
+      let result = await CreateProductRequest(FormValue, ObjectID);
+      if (result) {
+        navigate("/productList");
+      }
+    }
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <h3>Save Product</h3>
+                <hr className="bg-light" />
+
+                <div className="col-4 p-2">
+                  <label className="form-label">Product Name</label>
+                  <input
+                    onChange={(e) => {
+                      store.dispatch(
+                        OnChangeProductInput({
+                          Name: "Name",
+                          Value: e.target.value,
+                        })
+                      );
+                    }}
+                    value={FormValue.Name}
+                    className="form-control form-control-sm"
+                    type="text"
+                  />
+                </div>
+
+                <div className="col-4 p-2">
+                  <label className="form-label">Product Brand</label>
+                  <select
+                    onChange={(e) => {
+                      store.dispatch(
+                        OnChangeProductInput({
+                          Name: "BrandID",
+                          Value: e.target.value,
+                        })
+                      );
+                    }}
+                    value={FormValue.BrandID}
+                    className="form-select form-select-sm"
+                  >
+                    <option value="">Select Type</option>
+                    {BrandDropDown.map((item, i) => {
+                      return (
+                        <option key={i.toString()} value={item._id}>
+                          {item.Name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div className="col-4 p-2">
+                  <label className="form-label">Product Category</label>
+                  <select
+                    onChange={(e) => {
+                      store.dispatch(
+                        OnChangeProductInput({
+                          Name: "CategoryID",
+                          Value: e.target.value,
+                        })
+                      );
+                    }}
+                    value={FormValue.CategoryID}
+                    className="form-select form-select-sm"
+                  >
+                    <option value="">Select Type</option>
+                    {CategoryDropDown.map((item, i) => {
+                      return (
+                        <option key={i.toString()} value={item._id}>
+                          {item.Name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div className="col-4 p-2">
+                  <label className="form-label">Unit</label>
+                  <input
+                    onChange={(e) => {
+                      store.dispatch(
+                        OnChangeProductInput({
+                          Name: "Unit",
+                          Value: e.target.value,
+                        })
+                      );
+                    }}
+                    value={FormValue.Unit}
+                    className="form-control form-control-sm"
+                    type="text"
+                  />
+                </div>
+
+                <div className="col-4 p-2">
+                  <label className="form-label">Details</label>
+                  <input
+                    onChange={(e) => {
+                      store.dispatch(
+                        OnChangeProductInput({
+                          Name: "Details",
+                          Value: e.target.value,
+                        })
+                      );
+                    }}
+                    value={FormValue.Details}
+                    className="form-control form-control-sm"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-4 p-2">
+                  <button
+                    onClick={SaveChange}
+                    className="btn btn-sm my-3 btn-success"
+                  >
+                    Save Change
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCreateUpdate;
