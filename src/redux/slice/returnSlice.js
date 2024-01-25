b9bfd00b013d736aba01802f0cd72295f1dddf3b
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-26 01:01:07 +0600

    Add returnSlice for Redux functionality 🔧

---
+++ src/redux/slice/returnSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const returnSlice = createSlice({
+  name: "return",
+  initialState: {
+    List: [],
+    ListTotal: 0,
+    CustomerDropDown: [],
+    ProductDropDown: [],
+    ReturnFormValue: {
+      CustomerID: "",
+      VatTax: "",
+      Discount: "0",
+      OtherCost: "0",
+      ShippingCost: "",
+      GrandTotal: "",
+      Note: "Return Note...",
+    },
+    ReturnItemList: [],
+  },
+  reducers: {
+    SetReturnList: (state, action) => {
+      state.List = action.payload;
+    },
+    SetReturnListTotal: (state, action) => {
+      state.ListTotal = action.payload;
+    },
+    SetCustomerDropDown: (state, action) => {
+      state.CustomerDropDown = action.payload;
+    },
+    SetProductDropDown: (state, action) => {
+      state.ProductDropDown = action.payload;
+    },
+    OnChangeReturnInput: (state, action) => {
+      state.ReturnFormValue[`${action.payload.Name}`] = action.payload.Value;
+    },
+    SetReturnItemList: (state, action) => {
+      state.ReturnItemList.push(action.payload);
+    },
+    RemoveReturnItem: (state, action) => {
+      state.ReturnItemList.splice(action.payload, 1);
+    },
+  },
+});
+
+export const {
+  SetReturnList,
+  SetReturnListTotal,
+  SetCustomerDropDown,
+  SetProductDropDown,
+  OnChangeReturnInput,
+  SetReturnItemList,
+  RemoveReturnItem,
+} = returnSlice.actions;
+export default returnSlice.reducer;
