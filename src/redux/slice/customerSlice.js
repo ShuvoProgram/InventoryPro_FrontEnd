cd3a803ab7af2133ce235ed9b3319cf7ee443ff5
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-26 00:52:12 +0600

    Add customerSlice for Redux state management 🚀

---
+++ src/redux/slice/customerSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const customerSlice = createSlice({
+  name: "customer",
+  initialState: {
+    List: [],
+    ListTotal: 0,
+    FormValue: {
+      CustomerName: "",
+      Phone: "",
+      Email: "",
+      Address: "",
+    },
+  },
+  reducers: {
+    SetCustomerList: (state, action) => {
+      state.List = action.payload;
+    },
+    SetCustomerListTotal: (state, action) => {
+      state.ListTotal = action.payload;
+    },
+    SetOnChangeInput: (state, action) => {
+      state.FormValue[`${action.payload.Name}`] = action.payload.Value;
+    },
+    ResetFormValue: (state, action) => {
+      Object.keys(state.FormValue).forEach((i) => (state.FormValue[i] = ""));
+    },
+  },
+});
+
+export const {
+  SetCustomerList,
+  SetCustomerListTotal,
+  SetOnChangeInput,
+  ResetFormValue,
+} = customerSlice.actions;
+export default customerSlice.reducer;
